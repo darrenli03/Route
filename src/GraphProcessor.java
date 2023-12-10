4c7f93aa5b0bec0c12d9df0b8bb506865bed8ac6
kevin.han@duke.edu <kxh0423@gmail.com>
2023-12-10 14:05:30 -0500

    huh

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -48,7 +48,35 @@ public class GraphProcessor {
         Next n lines will be points with the format: point name [space] lat [space] long
         Next m lines will be edges with the format: first point [space] second vertex
         */
-        try{
+
+        Scanner s = new Scanner(file);
+            String first = s.nextLine();
+            String[] firstdata = first.split(" ");
+            int pts = Integer.parseInt(firstdata[0]);
+            int egs = Integer.parseInt(firstdata[1]);
+            Point[] ref = new Point[pts];
+            int ptnum = -1;
+            int ptnum2 = -2;
+
+            for(int i = 0; i < pts; i++)
+            {
+                s.nextLine();
+                Point input = new Point(s.nextDouble(), s.nextDouble());
+                myGraph.put(input, new HashSet<Point>());
+                ref[i] = input;
+            }
+
+            for(int j = 0; j < egs; j++)
+            {
+                s.nextLine();
+
+                //ptnum is the first point's number in the point reference array of the edge, ptnum2 is the second
+                ptnum = s.nextInt();
+                ptnum2 = s.nextInt();
+
+                myGraph.get(ref[ptnum]).add(ref[ptnum2]);
+                 myGraph.get(ref[ptnum2]).add(ref[ptnum]);}
+        /*try{
             Scanner s = new Scanner(file);
             String first = s.nextLine();
             String[] firstdata = first.split(" ");
@@ -79,7 +107,7 @@ public class GraphProcessor {
             }
         } catch (Exception e){
             throw new IOException("Could not read .graph file, or something else lmao idk");
-        }
+        }*/
 
     }
 
